9d5d8e61e6c03b6644f9ccd07979350e6be1c8f5
Aimer Neige <aimer.neige.soft@gmail.com>
2022-08-05 00:10:56 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# MiraiGo-module-random-mute
+
+ID: `com.aimerneige.random.mute`
+
+Module for [MiraiGo-Template](https://github.com/Logiase/MiraiGo-Template)
+
+## 功能
+
+- 在群聊中接受到指定消息时对消息发送者进行 0 ~ 2 小时的禁言。
+
+## 使用方法
+
+在适当位置引用本包
+
+```go
+package example
+
+imports (
+    // ...
+
+    _ "github.com/yukichan-bot-module/MiraiGo-module-random-mute"
+
+    // ...
+)
+
+// ...
+```
+
+如果需要自定义关键词，在你的 `application.yaml` 里填入配置（支持个关键词）：
+
+```yaml
+aimerneige:
+  random_mute:
+    keywords:
+      - ".ban.me"
+      - "随机禁言"
+```
+
+不填写未默认，填写后会覆盖默认关键词。
---
+++ mute.go
@@ -0,0 +1,78 @@
+package mute
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/Logiase/MiraiGo-Template/bot"
+	"github.com/Logiase/MiraiGo-Template/config"
+	"github.com/Logiase/MiraiGo-Template/utils"
+	"github.com/Mrs4s/MiraiGo/client"
+	"github.com/Mrs4s/MiraiGo/message"
+)
+
+var instance *mute
+var logger = utils.GetModuleLogger("com.aimerneige.random.mute")
+var keywordList []string
+
+type mute struct {
+}
+
+func init() {
+	instance = &mute{}
+	bot.RegisterModule(instance)
+}
+
+func (m *mute) MiraiGoModule() bot.ModuleInfo {
+	return bot.ModuleInfo{
+		ID:       "com.aimerneige.random.mute",
+		Instance: instance,
+	}
+}
+
+func (m *mute) Init() {
+	configKeywordList := config.GlobalConfig.GetStringSlice("aimerneige.random_mute.keywords")
+	if len(configKeywordList) == 0 {
+		keywordList = []string{".ban.me", "口球大礼包"}
+	} else {
+		for _, k := range configKeywordList {
+			keywordList = append(keywordList, k)
+		}
+	}
+}
+
+func (m *mute) PostInit() {
+}
+
+func (m *mute) Serve(b *bot.Bot) {
+	b.OnGroupMessage(func(c *client.QQClient, msg *message.GroupMessage) {
+		if isKeyword(msg.ToString()) {
+			groupMemberInfo, err := c.GetMemberInfo(msg.GroupCode, msg.Sender.Uin)
+			if err != nil {
+				logger.WithError(err).Errorf("Fail to get group member info.")
+				return
+			}
+			seed := rand.NewSource(time.Now().UnixNano())
+			rand := rand.New(seed)
+			muteDuration := rand.Intn(7200)
+			groupMemberInfo.Mute(uint32(muteDuration))
+		}
+	})
+}
+
+func (m *mute) Start(b *bot.Bot) {
+}
+
+func (m *mute) Stop(b *bot.Bot, wg *sync.WaitGroup) {
+	defer wg.Done()
+}
+
+func isKeyword(msg string) bool {
+	for _, s := range keywordList {
+		if s == msg {
+			return true
+		}
+	}
+	return false
+}
